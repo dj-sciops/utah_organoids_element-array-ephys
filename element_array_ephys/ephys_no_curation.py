aefda50da9d5035e33cea6358ebf3a0fe53bb08d
MilagrosMarin <milmarina02@gmail.com>
2024-08-01 13:58:54 +0100

    refactor: more concise `NotImplementedError` checks and error messages

--- element_array_ephys/ephys_no_curation.py
+++ element_array_ephys/ephys_no_curation.py
@@ -763,8 +763,19 @@ class CuratedClustering(dj.Imported):
                         "spike_depths": spike_depths,
                     }
                 )
-        else:  # read from kilosort outputs
-            raise NotImplementedError
+        elif (
+            not si_sorting_analyzer_dir.exists()
+            and (output_dir / sorter_name / "spikeinterface_report").exists()
+        ):
+            # Corresponds to a version of spikeinterface == 0.100.* since `sorting_analyzer` was introduced in version 0.101.*
+            raise NotImplementedError(
+                "Old version of SpikeInterface (== 0.100.*) not supported."
+            )
+        else:
+            # read from kilosort outputs
+            raise NotImplementedError(
+                "Functionality for reading from kilosort outputs not implemented yet."
+            )
 
         self.insert1(key)
         self.Unit.insert(units, ignore_extra_fields=True)
